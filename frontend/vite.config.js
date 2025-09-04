d486f40d455a6388974efb54602214ef67902fa3
Tomaz03 <e.ftomaz@gmail.com>
2025-09-04 14:15:54 -0300

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,6 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Ajuste o caminho do alias para que ele suba um nível (`..`)
-      // e então entre na pasta `src` do projeto.
-      // Isso corrige o problema do Render
-      '@': path.resolve(__dirname, '../src'),
-    },
-  },
   server: {
     port: 5173,
     host: '0.0.0.0',
@@ -27,6 +19,7 @@ export default defineConfig({
     },
   },
 } );
+
 
 
 
